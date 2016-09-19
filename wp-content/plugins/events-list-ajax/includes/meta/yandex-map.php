e88ca0830ef40d1b14aed762c695ac4f1c3ce451
Ilia Dudarev <del.ilyad@gmail.com>
2016-09-19 13:24:12 +0200

    Доделал яндекс карты.
    Отображение места на яндекс карте.
    Мелкие исправления форматирования.

--- wp-content/plugins/events-list-ajax/includes/meta/yandex-map.php
+++ wp-content/plugins/events-list-ajax/includes/meta/yandex-map.php
@@ -7,25 +7,55 @@
  *
  * @package TribeEventsCalendar
  */
-if ( empty( $map ) ) {
-	//return;
+
+$address = '';
+$location_parts = array( 'address', 'city', 'state', 'province', 'zip', 'country' );
+
+// Form the address string for the map
+foreach ( $location_parts as $val ) {
+    $address_part = call_user_func( 'tribe_get_' . $val, $venue_id );
+    if ( $address_part ) {
+        $address .= $address_part . ' ';
+    }
 }
 
 ?>
 
-<div id="events-list-yandex-map" class="tribe-events-venue-map">
+<div id="events-list-yandex-map">
+    
 </div>
 
 <script type="text/javascript">
+
 ymaps.ready(function() {
-	// Создание экземпляра карты и его привязка к контейнеру с заданным id ("map").
-    var myMap = new ymaps.Map('events-list-yandex-map', {
-        // При инициализации карты обязательно нужно указать её центр и коэффициент масштабирования.
-        center: [54.70453942733338,20.473791500000015], // калининград
-        zoom: 13
-    });
-	myMap.geoObjects.add(
-		new ymaps.Placemark([54.70453942733338,20.473791500000015], { hintContent: 'Калининград!'})
-	);
+    
+    var myGeocoder = ymaps.geocode("<?php echo $address;?>");
+    myGeocoder.then(
+        function (res) {
+            jQuery('#events-list-yandex-map').addClass('tribe-events-venue-map')
+            // Выбираем первый результат геокодирования.
+            var firstGeoObject = res.geoObjects.get(0);
+            // Координаты геообъекта.
+            var coords = firstGeoObject.geometry.getCoordinates()
+
+            // Создание экземпляра карты и его привязка к контейнеру с заданным id ("map").
+            var map = new ymaps.Map('events-list-yandex-map', {
+                // При инициализации карты обязательно нужно указать её центр и коэффициент масштабирования.
+                center: coords, // калининград
+                zoom: 14
+            });
+            // Добавляем первый найденный геообъект на карту.
+            map.geoObjects.add(firstGeoObject);
+            // Масштабируем карту на область видимости геообъекта.
+            
+            // Область видимости геообъекта.
+            var bounds = firstGeoObject.properties.get('boundedBy');
+            map.setBounds(bounds, {
+                // Проверяем наличие тайлов на данном масштабе.
+                checkZoomRange: true
+            });
+            
+        }
+    );
 });
 </script>
